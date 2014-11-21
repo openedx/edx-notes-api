3dd0846123a5ab82f65b46253cbdef1045f36cf1
Oleg Marshev <oleh.marshev@gmail.com>
2014-11-21 16:56:52 +0200

    Add ES setting to test.

--- notesserver/settings/test.py
+++ notesserver/settings/test.py
@@ -13,3 +13,6 @@ DATABASES = {
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 INSTALLED_APPS = ('django_nose',)
+
+ELASTICSEARCH_URL = 'http://127.0.0.1:9200'
+ELASTICSEARCH_INDEX = 'edx-notes-test'
