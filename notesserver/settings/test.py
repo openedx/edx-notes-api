3f736efad12d836ea9df628214bbb0a391f5c9da
Oleg Marshev <oleh.marshev@gmail.com>
2014-11-27 15:42:48 +0200

    Remove path dependency.

--- notesserver/settings/test.py
+++ notesserver/settings/test.py
@@ -1,12 +1,8 @@
 from .common import *
-from path import path
-
-TEST_ROOT = path("test_root")
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': TEST_ROOT / "db" / "notesserver.db",
     }
 }
 
