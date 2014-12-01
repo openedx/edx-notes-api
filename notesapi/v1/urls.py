75cad0b7b680c07cdd4151b8cd76608df27d4957
Oleg Marshev <oleh.marshev@gmail.com>
2014-12-01 15:43:02 +0200

    Use slash in search endpoint.

--- notesapi/v1/urls.py
+++ notesapi/v1/urls.py
@@ -6,6 +6,6 @@ from notesapi.v1.views import AnnotationListView, AnnotationDetailView, Annotati
 urlpatterns = patterns('',  # nopep8
     url(r'^annotations/$', AnnotationListView.as_view(), name='annotations'),
     url(r'^annotations/(?P<annotation_id>[a-zA-Z0-9_-]+)$', AnnotationDetailView.as_view(), name='annotations_detail'),
-    url(r'^search$', AnnotationSearchView.as_view(), name='annotations_search'),
+    url(r'^search/$', AnnotationSearchView.as_view(), name='annotations_search'),
     url(r'^status/$', RedirectView.as_view(url=reverse_lazy('status')), name='status'),
 )
