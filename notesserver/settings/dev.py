943234be7cf5cc6acb67e295a82ca31c5028737d
Oleg Marshev <oleh.marshev@gmail.com>
2014-12-01 15:24:11 +0200

    Add es to dev settings.

--- notesserver/settings/dev.py
+++ notesserver/settings/dev.py
@@ -1,3 +1,4 @@
+import annotator
 from annotator import es
 from .common import *
 
@@ -9,3 +10,14 @@ ELASTICSEARCH_INDEX = 'edx-notes-dev'
 # Overwrite default annotator-store elasticsearch settings.
 es.host = ELASTICSEARCH_URL
 es.index = ELASTICSEARCH_INDEX
+
+# Number of rows to return by default in result.
+RESULTS_DEFAULT_SIZE = 25
+
+# Max number of rows to return in result.
+RESULTS_MAX_SIZE = 250
+
+# Override default annotator-store elasticsearch settings.
+es.host = ELASTICSEARCH_URL
+es.index = ELASTICSEARCH_INDEX
+annotator.elasticsearch.RESULTS_MAX_SIZE = RESULTS_MAX_SIZE
