37024dea5cdff8280167ff1b79a11481e89e2ab9
Tim Babych <tim.babych@gmail.com>
2014-12-01 18:45:09 +0200

    remove permission work from initial PR

--- notesapi/v1/permissions.py
+++ notesapi/v1/permissions.py
@@ -1,14 +1,8 @@
 from rest_framework.permissions import BasePermission
-from provider.oauth2.models import AccessToken
 
 class HasAccessToken(BasePermission):
     """
-    Allow requests having valid AccessToken.
+    Allow requests having valid ID Token.
     """
     def has_permission(self, request, view):
-        try:
-            # expected HTTP Header "X-Annotator-Auth-Token: TOKEN"
-            AccessToken.objects.get_token(request.META["HTTP-X-ANNOTATOR-AUTH-TOKEN"].strip())
-            return True
-        except AccessToken.DoesNotExist:
-            return False
+        return True
--- notesserver/settings/common.py
+++ notesserver/settings/common.py
@@ -87,28 +87,12 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-# SERVICE_VARIANT specifies name of the variant used, which decides what JSON
-# configuration files are read during startup.
-SERVICE_VARIANT = os.environ.get('SERVICE_VARIANT', 'lms')
-
-# CONFIG_ROOT specifies the directory where the JSON configuration
-# files are expected to be found.
-CONFIG_ROOT = os.environ.get('CONFIG_ROOT', "/edx/app/edxapp")
-
-# CONFIG_PREFIX specifies the prefix of the JSON configuration files,
-# based on the service variant. If no variant is use, don't use a
-# prefix.
-CONFIG_PREFIX = SERVICE_VARIANT + "." if SERVICE_VARIANT else ""
-
-try:
-    with open(os.path.join(CONFIG_ROOT, CONFIG_PREFIX + "auth.json")) as auth_file:
-        AUTH_TOKENS = json.load(auth_file)
-        DATABASES = AUTH_TOKENS['DATABASES']
-except IOError:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:'
-        }
-    }
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    'x-annotator-auth-token',
+)
