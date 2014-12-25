c9626624e64da23ddec57fbd81cfb3df8a1cd8b6
Tim Babych <tim.babych@gmail.com>
2014-12-25 16:09:05 +0200

    add elasticsearch searcher

--- notesserver/settings/test.py
+++ notesserver/settings/test.py
@@ -21,3 +21,29 @@ es.host = ELASTICSEARCH_URL
 es.index = ELASTICSEARCH_INDEX
 annotator.elasticsearch.RESULTS_MAX_SIZE = RESULTS_MAX_SIZE
 ###############################################################################
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+            'formatter': 'standard',
+        },
+    },
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s %(levelname)s %(process)d [%(name)s] %(filename)s:%(lineno)d - %(message)s',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
