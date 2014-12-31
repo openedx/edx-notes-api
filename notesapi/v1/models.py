5323ac5b42c80be252c692036a4e70fc04491106
Tim Babych <tim.babych@gmail.com>
2014-12-31 19:48:28 +0200

    remove doc conflict

--- notesapi/v1/models.py
+++ notesapi/v1/models.py
@@ -123,17 +123,11 @@ class NoteMappingType(MappingType, Indexable):
     @staticmethod
     def process_result(data):
         """
-<<<<<<< HEAD
-        Prepares result for response.
-
-        Also prepares ranges field and highlighting.
-=======
         Unlistifies the result and replaces `text` with highlihted one
 
         Unlistification: ElasticUtils returns data as [{field:value,..}..] which is not what needed.
         this function reverses the effect to get the original value.
         Also filed https://github.com/mozilla/elasticutils/pull/285 to make it unnecessary.
->>>>>>> specify ElastiUtils more precisely, add docstrings
         """
         for i, item in enumerate(data):
             if isinstance(item, dict):
