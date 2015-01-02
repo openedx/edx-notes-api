264abc94c4775c559e639ea06cbb51577a6f7ab1
Oleg Marshev <oleh.marshev@gmail.com>
2015-01-02 14:13:05 +0200

    travis.

--- notesserver/settings/test.py
+++ notesserver/settings/test.py
@@ -11,3 +11,27 @@ DISABLE_TOKEN_CHECK = False
 INSTALLED_APPS += ('django_nose',)
 
 ES_INDEXES = {'default': 'notes_index_test'}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'elasticsearch.trace': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False
+        }
+    },
+}
