8c64c644a4a87caa33117e6dd7e4b5e6a17dbabd
Edward Zarecor <ed@edx.org>
2015-01-06 21:01:35 -0500

    / operator requires a path, fails on two string

--- notesserver/settings/yaml_config.py
+++ notesserver/settings/yaml_config.py
@@ -1,7 +1,8 @@
 import yaml
 
 from .common import *  # pylint: disable=unused-wildcard-import, wildcard-import
-
+from path import path
+from django.core.exceptions import ImproperlyConfigured
 
 ###############################################################################
 # Explicitly declare here in case someone changes common.py.
@@ -11,7 +12,12 @@ TEMPLATE_DEBUG = False
 DISABLE_TOKEN_CHECK = False
 ###############################################################################
 
-CONFIG_ROOT = os.environ.get('EDXNOTES_CONFIG_ROOT')
+EDXNOTES_CONFIG_ROOT = os.environ.get('EDXNOTES_CONFIG_ROOT')
+
+if not EDXNOTES_CONFIG_ROOT:
+    raise ImproperlyConfigured("EDXNOTES_CONFIG_ROOT must be defined in the environment.")
+                                      
+CONFIG_ROOT = path(EDXNOTES_CONFIG_ROOT)
 
 with open(CONFIG_ROOT / "edx-notes-api.yml") as yaml_file:
     config_from_yaml = yaml.load(yaml_file)
