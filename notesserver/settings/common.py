5b26d4511391dccdd106d00dc47476cec7e61cc4
Diana Huang <dkh@edx.org>
2020-03-13 16:36:07 -0400

    Attempt to upgrade ES.

--- notesserver/settings/common.py
+++ notesserver/settings/common.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
+    'rest_framework_jwt',
+    'rest_framework_jwt.blacklist',
     'corsheaders',
     'haystack',
     'notesapi.v1',
