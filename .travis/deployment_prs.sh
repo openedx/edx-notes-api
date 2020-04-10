d44d7233f4d19201c110ee53fe37759139b03f6e
Fred Smith <derf@edx.org>
2020-04-09 21:32:46 -0400

    travis git PR fixes

--- .travis/deployment_prs.sh
+++ .travis/deployment_prs.sh
@@ -14,7 +14,7 @@ cd edx-internal
 
 # stage
 git checkout -b edx-deployment/stage/$TRAVIS_COMMIT
-sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic" argocd/applications/edx-notes-api/stage/kustomization.yaml
+sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic/" argocd/applications/edx-notes-api/stage/kustomization.yaml
 git commit -a -m "edx-notes-api stage deploy: $TRAVIS_COMMIT_MESSAGE" --author "Travis CI Deployment automation <admin@edx.org>"
 git push --set-upstream origin edx-deployment/stage/$TRAVIS_COMMIT
 ../hub-linux*/bin/hub pull-request -m "Edx-notes-api stage deploy: $TRAVIS_COMMIT_MESSAGE"
@@ -22,7 +22,7 @@ git push --set-upstream origin edx-deployment/stage/$TRAVIS_COMMIT
 # prod
 git checkout master
 git checkout -b edx-deployment/prod/$TRAVIS_COMMIT
-sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic" argocd/applications/edx-notes-api/prod/kustomization.yaml
+sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic/" argocd/applications/edx-notes-api/prod/kustomization.yaml
 git commit -a -m "edx-notes-api prod deploy: $TRAVIS_COMMIT_MESSAGE" --author "Travis CI Deployment automation <admin@edx.org>"
 git push --set-upstream origin edx-deployment/prod/$TRAVIS_COMMIT
 ../hub-linux*/bin/hub pull-request -m "Edx-notes-api prod deploy: $TRAVIS_COMMIT_MESSAGE"
@@ -30,7 +30,7 @@ git push --set-upstream origin edx-deployment/prod/$TRAVIS_COMMIT
 # edge
 git checkout master
 git checkout -b edx-deployment/edge/$TRAVIS_COMMIT
-sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic" argocd/applications/edx-notes-api/edge/kustomization.yaml
+sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic/" argocd/applications/edx-notes-api/edge/kustomization.yaml
 git commit -a -m "edx-notes-api edge deploy: $TRAVIS_COMMIT_MESSAGE" --author "Travis CI Deployment automation <admin@edx.org>"
 git push --set-upstream origin edx-deployment/edge/$TRAVIS_COMMIT
 ../hub-linux*/bin/hub pull-request -m "Edx-notes-api edge deploy: $TRAVIS_COMMIT_MESSAGE"
