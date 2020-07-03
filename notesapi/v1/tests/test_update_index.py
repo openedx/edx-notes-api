68bd82e16a77277078401c6d13530a7bf336042e
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-07-03 18:07:55 +0300

    OeX_ES-23: Updates notesapi tests.

--- notesapi/v1/tests/test_update_index.py
+++ notesapi/v1/tests/test_update_index.py
@@ -1,11 +1,10 @@
-import datetime
 from unittest import skipIf
 
 from django.conf import settings
 from django.core.management import call_command
 from django.urls import reverse
 from django.db.models import signals
-
+from django_elasticsearch_dsl.registries import registry
 import factory
 
 from .test_views import BaseAnnotationViewTests
@@ -23,29 +22,20 @@ class UpdateIndexTest(BaseAnnotationViewTests):
         Ensure we can update index with created notes within specific
         period of time.
         """
-        start = datetime.datetime.now()
         self._create_annotation(text=u'First note')
-
-        second_start = datetime.datetime.now()
         self._create_annotation(text=u'Second note')
-        second_end = datetime.datetime.now()
-
         self._create_annotation(text=u'Third note')
-        end = datetime.datetime.now()
 
         results = self._get_search_results(text='note')
-        self.assertDictContainsSubset({'rows': [], 'total': 0}, results)
 
-        # When second note was created.
-        call_command('update_index', start_date=second_start.isoformat(), end_date=second_end.isoformat(), verbosity=0)
-        results = self._get_search_results(text='note')
-        self.assertEqual(results['total'], 1)
-        self.assertEqual(results['rows'][0]['text'], 'Second note')
+        self.assertEqual(results['total'], 0)
+        self.assertEqual(results['rows'], [])
 
-        # All notes.
-        call_command('update_index', start_date=start.isoformat(), end_date=end.isoformat(), verbosity=0)
+        call_command('search_index', '--populate')
+
         results = self._get_search_results(text='note')
         self.assertEqual(results['total'], 3)
+        self.assertEqual(results['rows'][0]['text'], 'First note')
 
     @factory.django.mute_signals(signals.post_delete)
     def test_delete(self):
@@ -65,16 +55,11 @@ class UpdateIndexTest(BaseAnnotationViewTests):
         response = self.client.delete(url, self.headers)
 
         # Delete second note.
-        second_start = datetime.datetime.now()
         url = reverse('api:v1:annotations_detail', kwargs={'annotation_id': second_note['id']})
         response = self.client.delete(url, self.headers)
-        second_end = datetime.datetime.now() + datetime.timedelta(minutes=10)
 
         # Try to update when only second note was deleted.
-        call_command(
-            'update_index', remove=True, start_date=second_start.isoformat(),
-            end_date=second_end.isoformat(), verbosity=0
-        )
+        call_command('search_index', '--rebuild', '-f')
         results = self._get_search_results(text='note')
 
         # When remove flag is provided, start and end flags do not play any role.
