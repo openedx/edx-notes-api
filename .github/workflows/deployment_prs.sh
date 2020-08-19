db4db7554e66dff02588d7c7d88fb50d0ef5fd96
syed-awais-ali <awaisalisabir@gmail.com>
2020-08-19 17:30:40 +0500

    fixed GH action to push docker image

--- .github/workflows/deployment_prs.sh
+++ .github/workflows/deployment_prs.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
 export GITHUB_USER='edx-deployment'
-export GITHUB_TOKEN=$GH_TOKEN
+export GITHUB_ACCESS_TOKEN=$GH_TOKEN
 export REPO_NAME='edx-notes-api'
 export GITHUB_EMAIL='edx-deployment@edx.org'
 
 export GITHUB_UPSTREAM_PR_NUMBER=$(echo $TRAVIS_COMMIT_MESSAGE | sed -e 's/.*#//' -e 's/ .*//');
 
 cd ..
-git clone https://edx-deployment:${GITHUB_TOKEN}@github.com/edx/edx-internal
+git clone https://edx-deployment:${GITHUB_ACCESS_TOKEN}@github.com/edx/edx-internal
 
 # install hub
 curl -L -o hub.tgz https://github.com/github/hub/releases/download/v2.14.2/hub-linux-amd64-2.14.2.tgz
@@ -51,3 +51,4 @@ sed -i -e "s/newTag: .*/newTag: $TRAVIS_COMMIT-newrelic/" argocd/applications/${
 git commit -a -m "${REPO_NAME} edge deploy: $TRAVIS_COMMIT_MESSAGE" --author "Travis CI Deployment automation <admin@edx.org>"
 git push --set-upstream origin edx-deployment/edge/$TRAVIS_COMMIT
 ../hub-linux*/bin/hub pull-request -m "${REPO_NAME} edge deploy: $TRAVIS_COMMIT_MESSAGE" -m "Edge environment deployment of https://github.com/edx/${REPO_NAME}/pull/$GITHUB_UPSTREAM_PR_NUMBER" -m "Review and merge this PR to deploy your code to edge.edx.org"
+
