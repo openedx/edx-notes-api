2e98c2f91c96aca3bee9b9ed85f1b6f279a8fb83
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-09-15 09:45:09 +0300

    OeX_ES-55: Creates a custom class 'Request' which implements Specifies custom behavior of the standard Django request class. Uses 'duck typing' pattern.

--- notesapi/v1/utils.py
+++ notesapi/v1/utils.py
@@ -3,14 +3,15 @@ Utilities for notes api application
 """
 
 from django.conf import settings
+from django.http import QueryDict
 from rest_framework.response import Response
-from rest_framework.utils.serializer_helpers import ReturnList
 
 
 class NotesPaginatorMixin:
     """
     Student Notes Paginator Mixin
     """
+
     page_size = settings.DEFAULT_NOTES_PAGE_SIZE
     page_size_query_param = "page_size"
 
@@ -18,12 +19,50 @@ class NotesPaginatorMixin:
         """
         Annotate the response with pagination information.
         """
-        return Response({
-            'start': (self.page.number - 1) * self.get_page_size(self.request),
-            'current_page': self.page.number,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'total': self.page.paginator.count,
-            'num_pages': self.page.paginator.num_pages,
-            'rows': data
-        })
+        return Response(
+            {
+                'start': (self.page.number - 1) * self.get_page_size(self.request),
+                'current_page': self.page.number,
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'total': self.page.paginator.count,
+                'num_pages': self.page.paginator.num_pages,
+                'rows': data,
+            }
+        )
+
+
+def dict_to_querydict(dict_):
+    """
+    Converts a dict value into a Django's QueryDict object.
+    """
+    query_dict = QueryDict("", mutable=True)
+    for name, value in dict_.items():
+        if isinstance(name, list):
+            query_dict.setlist(name, value)
+        else:
+            query_dict.appendlist(name, value)
+    query_dict._mutable = False
+    return query_dict
+
+
+class Request:
+    """
+    Specifies custom behavior of the standard Django request class.
+
+    Implementation of the `duck typing` pattern.
+    Using an object of class `Request` allows you to define the desired logic,
+    which will be different from Django's Request.
+    Those program components that are expecting a Django request,
+    but they will use `Request` - will not notice the substitution at all.
+    """
+
+    def __init__(self, query_params):
+        self._query_params = query_params
+
+    @property
+    def query_params(self):
+        """
+        Returns the Django's QueryDict object, which presents request's query params.
+        """
+        return dict_to_querydict(self._query_params)
