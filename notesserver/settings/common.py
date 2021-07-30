3d83e2e20f1fc600e3d4661ea91312a6ff92d4be
M. Zulqarnain <zulqarnain.mailbox@gmail.com>
2021-07-30 18:17:47 +0500

    feat: Django 3.2 Support

--- notesserver/settings/common.py
+++ notesserver/settings/common.py
@@ -35,6 +35,11 @@ RESULTS_MAX_SIZE = 250
 
 ROOT_URLCONF = 'notesserver.urls'
 
+DEFAULT_HASHING_ALGORITHM = "sha1"
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+
 MIDDLEWARE = (
     'edx_django_utils.cache.middleware.RequestCacheMiddleware',
     'corsheaders.middleware.CorsMiddleware',
