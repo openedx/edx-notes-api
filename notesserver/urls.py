1348af7d3d6744228ab647940bba81f183fb5f37
Chris Deery <3932645+cdeery@users.noreply.github.com>
2021-10-13 18:05:14 -0400

    feat: [AA-1069] Add robots.txt file

--- notesserver/urls.py
+++ notesserver/urls.py
@@ -5,6 +5,7 @@ import notesserver.views
 urlpatterns = [
     url(r'^heartbeat/$', notesserver.views.heartbeat, name='heartbeat'),
     url(r'^selftest/$', notesserver.views.selftest, name='selftest'),
+    url(r'^robots.txt$', notesserver.views.robots, name='robots'),
     url(r'^$', notesserver.views.root, name='root'),
     url(r'^api/', include('notesapi.urls', namespace='api')),
 ]
