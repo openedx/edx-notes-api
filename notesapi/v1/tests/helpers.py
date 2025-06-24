f19301ff9a4879becfc83b7d0f0e4e6d2f4176de
Irtaza Akram <51848298+irtazaakram@users.noreply.github.com>
2025-06-24 13:18:44 +0500

    fix: add django 5.2 support (#492)

--- notesapi/v1/tests/helpers.py
+++ notesapi/v1/tests/helpers.py
@@ -1,12 +1,12 @@
 from calendar import timegm
-from datetime import datetime, timedelta
+from datetime import UTC, datetime, timedelta
 
 import jwt
 from django.conf import settings
 
 
 def get_id_token(user):
-    now = datetime.utcnow()
+    now = datetime.now(UTC)
     return jwt.encode({
         'aud': settings.CLIENT_ID,
         'sub': user,
